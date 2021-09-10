b482227dcfd99983c017973e6104259175bcf7b2
suryakumar265 <90467254+suryakumar265@users.noreply.github.com>
2021-09-10 21:21:52 +0530

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,11 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  title: 'Legendary Tricks',
+  description: 'Get Free Udemy Courses. Download all the demanding and trending courses for FREE. Get 100% Discount Coupons.',
+  keywords: ['courses', 'course', 'udemy', 'udemy courses', 'udemy course', 'udemy tutorials', 'free courses', 'free course site', 'udemy course free download', 'free tuts download', 'udemy discount coupons', 'udemy coupons', 'unacadrmy courses'],
   // your site url without trailing slash
-  url: 'https://vredeburg.netlify.app',
+  url: 'https://legendary-learning.netlify.app',
   // how many posts you want to show for each page
   paginate: 6
   // if you want to add disqus to your site
-  // disqusShortname: "your-shortname"
+  // disqusShortname: "legendary-learning"
 };
